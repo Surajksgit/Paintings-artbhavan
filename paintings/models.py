8ab26e1671c486692e86650e21a2b0befe56e342
suraj <surajkspavi531@gmail.com>
2025-04-20 12:01:14 +0530

    correctedd.....

--- paintings/models.py
+++ paintings/models.py
@@ -60,17 +60,3 @@ class Artwork(models.Model):
         return self.title
     
 # Cart------------------------------------>
-class Cart(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='cart')
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def total_price(self):
-        return sum(item.total_price() for item in self.items.all())
-
-class CartItem(models.Model):
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
-    artwork = models.ForeignKey(Artwork, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(default=1)
-
-    def total_price(self):
-        return self.artwork.price * self.quantity
